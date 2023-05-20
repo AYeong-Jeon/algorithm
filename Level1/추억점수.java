263b2a41747ccf305ba34d800efc183a24eba0a7
AYeong-Jeon <ay98923@naver.com>
2023-05-21 01:24:07 +0900

    20230520

--- Level1/추억점수.java
+++ Level1/추억점수.java
@@ -5,7 +5,7 @@ import java.util.*;
 //https://school.programmers.co.kr/learn/courses/30/lessons/176963
 
 public class 추억점수 {
-	public int[] solution(String[] name, int[] yearning, String[][] photo) {
+	public int[] solution1(String[] name, int[] yearning, String[][] photo) {
 		int[] answer = {};
 		Map<String, Integer> map = new HashMap<>();
 
@@ -23,6 +23,22 @@ public class 추억점수 {
 				num += map.get(photo[i][j]);
 			}
 			answer[i] = num;
+		}
+
+		return answer;
+	}
+
+
+	/*-------------------다른 사람 코드 보고 고친 부분----------------------*/
+	public int[] solution2(String[] name, int[] yearning, String[][] photo) {
+		int[] answer = new int[photo.length];
+
+		for(int i=0 ; i<photo.length ; i++) {
+			for(int j=0 ; j<photo[i].length ; j++) {
+				if(Arrays.asList(name).indexOf(photo[i][j]) != -1) {
+					answer[i] += yearning[Arrays.asList(name).indexOf(photo[i][j])];
+				}
+			}
 		}
 
 		return answer;
