d0482ba499e0d83341c59b939e2591cd04361348
AYeong-Jeon <ay98923@naver.com>
2023-07-11 17:17:19 +0900

    20230630 트리, 정렬

---
+++ 자료구조/binary_search/수찾기.java
@@ -0,0 +1,24 @@
+package 자료구조.binary_search;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//https://www.acmicpc.net/problem/1920
+
+public class 수찾기 {
+
+	@Test
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+
+		System.out.println(sb);
+
+	}
+}
--- 자료구조/정렬/수정렬하기.java
+++ 자료구조/정렬/수정렬하기.java
@@ -35,9 +35,7 @@ class 수정렬하기 {
 	@Test
 	public static void solution2() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringBuilder sb = new StringBuilder();
-
 		int N = Integer.parseInt(br.readLine());
 
 		/*
@@ -46,12 +44,12 @@ class 수정렬하기 {
 		*/
 		boolean[] arr = new boolean[2001];
 
-		for(int i = 0; i < N; i++) {
+		for(int i=0 ; i<N ; i++) {
 			arr[Integer.parseInt(br.readLine()) + 1000] = true;
 		}
 
 		// 정렬 과정이 따로 필요 없음
-		for(int i = 0; i < 2001; i++) {
+		for(int i=0 ; i<2001 ; i++) {
 			if(arr[i]) {
 				sb.append(i - 1000).append('\n');
 			}
