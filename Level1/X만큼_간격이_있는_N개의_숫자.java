6cb82e2b47445682cd778be4e51fbceed4ff2729
AYeong-Jeon <ay98923@naver.com>
2024-08-02 17:53:42 +0900

    임시커밋

---
+++ Level1/X만큼_간격이_있는_N개의_숫자.java
@@ -0,0 +1,11 @@
+package Level1;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12954
+
+public class X만큼_간격이_있는_N개의_숫자 {
+    public long[] solution(int x, int n) {
+        long[] answer = {};
+
+        return answer;
+    }
+}
