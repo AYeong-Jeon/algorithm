1b12e58c96ab63f950e66603a39f51e70e456769
AYeong-Jeon <dkdud98923@gmail.com>
2024-08-03 01:13:54 +0900

    240802

--- Level1/X만큼_간격이_있는_N개의_숫자.java
+++ Level1/X만큼_간격이_있는_N개의_숫자.java
@@ -2,9 +2,16 @@ package Level1;
 
 //https://school.programmers.co.kr/learn/courses/30/lessons/12954
 
+import java.util.ArrayList;
+
 public class X만큼_간격이_있는_N개의_숫자 {
     public long[] solution(int x, int n) {
-        long[] answer = {};
+        long[] answer = new long[n];
+        answer[0] = x;
+
+        for(int i=1 ; i<n ; i++) {
+            answer[i] = answer[i-1]+x;
+        }
 
         return answer;
     }
