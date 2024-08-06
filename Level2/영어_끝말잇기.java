2647d1745540601ec7f36ae25e06f398d7f7347e
AYeong-Jeon <ay98923@naver.com>
2024-08-06 20:46:58 +0900

    임시커밋

--- Level2/영어_끝말잇기.java
+++ Level2/영어_끝말잇기.java
@@ -1,9 +1,39 @@
 package Level2;
 
-public class 영어_끝말잇기 {
-    public int[] solution(int n, String[] words) {
-        int[] answer = {};
+//https://school.programmers.co.kr/learn/courses/30/lessons/12981
 
+import java.util.LinkedList;
+
+public class 영어_끝말잇기 {
+    public static void main(String[] args) {
+        int[] a = solution(3, new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"});
+    }
+    public static int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        int count = 1;
+        int a=0, b=0;
+        boolean isDrop = false;
+
+        LinkedList<String> wordList = new LinkedList<>();
+        wordList.add(words[0]);
+        for (int i=1 ; i<words.length ; i++) {
+            count++;
+            char c = new StringBuilder().append(wordList.get(i-1)).reverse().charAt(0);
+            if (words[i].startsWith(String.valueOf(c)) && !wordList.contains(words[i])) {
+                wordList.add(words[i]);
+            } else {
+                isDrop = true;
+                break;
+            }
+        }
+
+        if(isDrop) {
+            a = count / n;
+            b = count % n;
+            if(b==0) b = a;
+        }
+        answer[0] = a;
+        answer[1] = b;
 
         return answer;
     }
