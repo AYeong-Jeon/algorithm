d0d4ea0a113bb8b17e4078a92133612ac22811e9
AYeong-Jeon <dkdud98923@gmail.com>
2024-08-18 00:56:45 +0900

    0817

--- Level1/행렬의_덧셈.java
+++ Level1/행렬의_덧셈.java
@@ -1,6 +1,8 @@
 package Level1;
 
-public class 행렬_더하기 {
+//https://school.programmers.co.kr/learn/courses/30/lessons/12950
+
+public class 행렬의_덧셈 {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr1[0].length];
         for(int i=0 ; i<arr1.length ; i++) {
