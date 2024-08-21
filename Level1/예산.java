a74738f63174db79aae994b28c53e512d1a9ab62
AYeong-Jeon <dkdud98923@gmail.com>
2024-08-21 23:52:05 +0900

    20240821

---
+++ Level1/예산.java
@@ -0,0 +1,20 @@
+package Level1;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12982
+
+import java.util.Arrays;
+
+public class 예산 {
+    public int solution(int[] d, int budget) {
+        int count = 0;
+        Arrays.sort(d);
+
+        for(int i=0 ; i<d.length ; i++) {
+            budget -= d[i];
+            if(budget<0) return count;
+            count++;
+        }
+
+        return count;
+    }
+}
